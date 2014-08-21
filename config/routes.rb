639a8f778ee876afce71240aec634a919bba9be0
ghislain99 <ghislain.touraine@gmail.com>
2014-08-21 15:28:01 +0200

    Users tests and db validation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Pampa::Application.routes.draw do
+	resources :users
+	
 	get '/', :to => 'pages#home'
 	get '/contact', :to => 'pages#contact'
 	get '/about',   :to => 'pages#about'
@@ -7,8 +9,6 @@ Pampa::Application.routes.draw do
 	get "users/new"
 	get '/signup',  :to => 'users#new'
 	
-	resources :users
-	  
 	root :to => 'pages#home'
  
 end
